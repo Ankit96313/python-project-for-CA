4889a528bf731de823b0172c40839fa61c299e35
Ankit <u99050@gmail.com>
2022-11-26 22:17:39 +0530

    comment1

--- project.py
+++ project.py
@@ -1,7 +1,13 @@
-#project
-user_input=str(input("Enter a Sentence: "))
-text=user_input.split()
-a=''
-for i in text:
-    a=a+str(i[0]).upper()
-print(a)
+#Acronym Program
+#Taking input from user
+Take_input_from_user=str(input("Enter a Sentence: "))
+#making list of words
+text=Take_input_from_user.split()
+#Creating empty string
+b=''
+#Acessing each word one by one using for loop
+for j in text:
+#Taking first letter of word and converting to uppercase 
+    b=b+str(j[0]).upper()
+#Printing Acronym
+print(b)
